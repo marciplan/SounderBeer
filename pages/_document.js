db318e83d72292e05521746eea6320f77c8a7060
marciplan <ik@marcianoplanque.nl>
2021-03-15 22:30:54 +0100

    Testing, Testing

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,13 @@ class MyDocument extends Document {
       <Html>
         <Head>
         <link href="https://fonts.googleapis.com/css2?family=Manrope&display=swap" rel="stylesheet" />
+        <meta property="og:title" content="The Rock" />
+        <meta property="og:type" content="video.movie" />
+        <meta property="og:url" content="http://www.imdb.com/title/tt0117500/" />
+        <meta
+          property="og:image"
+          content="http://ia.media-imdb.com/images/rock.jpg"
+        />
         </Head>
         <body>
           <Main />
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,12 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import { FiExternalLink} from "react-icons/fi";
 import Typewriter from 'typewriter-effect';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Futures Literacy is coming soon</title>
+        <title>Sounder, Beer. What's not to like?</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -16,21 +15,17 @@ export default function Home() {
 <h1 className={styles.title}>
   <Typewriter
   options={{
-  strings: ['We use the future every day.', 'We predict, fear, hope and dream.', 'This has a large impact on how we see the present', 'and yet, we know very little about how and why we use the future.', 'It is time for Futures Literacy.'],
+  strings: ['Hello.', 'Wanna 🍻?', '...', 'or maybe 🍷?', '...or ☕️ -- sure?', '...', 'Soon 💓'],
   autoStart: true,
-  deleteSpeed: 50,
+  deleteSpeed: 100,
   loop: true,
   pauseFor: 1000
   }}
 />
 </h1>
-
-        <div className={styles.holder}>
-        <a href="https://www.hanze.nl/eng/research/overviews/FutureLiteracyKnowLabs" target="_blank" className={styles.code}> <span style={{marginRight: "8px", textDecoration: "underline"}}>Futures Literacy is launching soon. In the mean time, learn more via Hanze UAS website</span><FiExternalLink /></a>
-        </div>
       </main>
 
-      <footer className={styles.footer}>&copy; 2021 Futures Literacy |  <a href="mailto: info@futuresliteracy.net">info@futuresliteracy.net</a></footer>
+      <footer className={styles.footer}>&copy; 2021 | A silly something from Sounder, for Sounder.</footer>
     </div>
   )
 }
